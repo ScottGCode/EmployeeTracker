0bf8edf201d2cf9e21f6c5d9b2f48499e146bff9
ScottGCode <scottrutherfords@gmail.com>
2023-11-09 15:44:32 -0700

    added employee database

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS title; 
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30)
+);
+
+CREATE TABLE title (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR (30),
+    last_name VARCHAR(30),
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES title(id) ON DELETE SET NULL
+    manager_id INT
+);
---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+INSERT INTO department (department_name) 
+VALUES  ('Sales'),
+        ('Engineering'),
+        ('Finance'),
+        ('Legal'),
+        ('Development');
+
+INSERT INTO title (title, salary, department_id)
+VALUES  ('Sales Lead', '100000', 1),
+        ('Salesperson', '80000', 1),
+        ('Lead Engingeer', '150000', 2),
+        ('Software Engineer', '120000' 2),
+        ('Account Manager', '160000', 3),
+        ('Accountant', '125000', 3),
+        ('Legal Team Lead', '250000', 4),
+        ('Lawyer', '190000', 4),
+        ('Senior Developer' '1000000', 5),
+        ('Junior Developer', '25000', 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Bill', 'Nye', '1', NULL),
+        ('John', 'Doe', '2', '1'),
+        ('Mike', 'Makalu', '3', NULL),
+        ('Tony', 'Allen', '4', '3'),
+        ('Tim', 'Toolman', '5', NULL),
+        ('Ashley', 'Rodriguez', '6', '5'),
+        ('Sue', 'Lawson', '7', NULL),
+        ('Robert', 'Lauer', '8', '7'),
+        ('Ronson', 'Brother', '9', NULL),
+        ('Scott', 'Rutherford', '10', '9');
