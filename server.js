c907fa641646dc0b3f8592940083db9fe9488727
ScottGCode <scottrutherfords@gmail.com>
2023-11-09 16:54:02 -0700

    added prompts and choices

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Import packages.
 const inquirer = require('inquirer');
 const mysql = require('mysql2'); 
-const cfonts = require('cfonts');
+const cFonts = require('cfonts');
 
 // MySQL connection to database
 const db = mysql.createConnection({
@@ -13,14 +13,24 @@ console.log('Connected to the company database!')
 );
 
 // ASCII font using NPM cfonts.
-cfonts.say('Employee Manager', {
-	font: 'block',              // define the font face
-	align: 'left',              // define text alignment
-	colors: ['green', 'whiteBright'],         // define all colors
-	background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
-	letterSpacing: 1,           // define letter spacing
-	lineHeight: 1,              // define the line height
-	space: true,                // define if the output text should have empty lines on top and on the bottom
-	maxLength: '0',             // define how many character can be on one line
-	env: 'node'                 // define the environment cfonts is being executed in
+cFonts.say('Employee Manager', {
+	font: 'block',
+	align: 'left',              
+	colors: ['green', 'whiteBright'],
+	background: 'transparent',  
+	letterSpacing: 1,           
+	lineHeight: 1,              
+	space: true,                
+	maxLength: '0',             
+	env: 'node'                 
 });
+
+const init = function () {
+    inquirer.prompt({
+        type: 'list',
+        name: 'database',
+        message: 'What would you like to do?',
+        choice: ['View All Employees', 'Add Employee', 'Update Employee Job Title', 'View All Job Titles', 'Add Job Title', 'View All Departments', 'Add Department']
+    })
+
+}
