477273de87d0e874c552b1ac71ce3949414bd569
ScottGCode <scottrutherfords@gmail.com>
2023-11-10 10:18:45 -0700

    added asynchronous functions

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Import packages.
 const inquirer = require('inquirer');
-const mysql = require('mysql2'); 
+const mysql = require('mysql2/promise'); 
 const cFonts = require('cfonts');
 
 // MySQL connection to database
@@ -8,9 +8,13 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'company_db'
-},
-console.log('Connected to the company database!')
-);
+}).then ((connection) => {
+    console.log('Connected to the company database!');
+    return connection;
+}).catch ((error) => {
+    console.error('Error connecting to the database:', error.message)
+    throw error;
+});
 
 // ASCII font using NPM cfonts.
 cFonts.say('Employee Manager', {
@@ -25,12 +29,65 @@ cFonts.say('Employee Manager', {
 	env: 'node'                 
 });
 
-const init = function () {
+const choices = {
+    'View All Employees': viewAllEmployees,
+    'Add Employee': addEmployee,
+    'Update Employee Job Title': updateEmployeeJobTitle,
+    'View All Job Titles': viewAllJobTitles,
+    'Add Job Title': addJobTitle,
+    'View All Departments': viewAllDepartments,
+    'Add Department': addDepartment,
+};
+
+function promptUser() {
     inquirer.prompt({
         type: 'list',
         name: 'database',
         message: 'What would you like to do?',
-        choice: ['View All Employees', 'Add Employee', 'Update Employee Job Title', 'View All Job Titles', 'Add Job Title', 'View All Departments', 'Add Department']
-    })
+        choices: Object.keys(choices),
+    }).then(response => {
+        const selectedAction = actions[response.database];
+        if (selectedAction) {
+            selectedAction();
+        } else {
+            console.log('Invalid choice');
+        }
+    });
+}
+
+async function viewAllEmployees() {
+    const connection = awaid db;
 
 }
+
+async function addEmployee() {
+    const connection = awaid db;
+
+}
+
+async function updateEmployeeJobTitle() {
+    const connection = awaid db;
+
+}
+
+async function viewAllJobTitles() {
+    const connection = awaid db;
+
+}
+
+async function addJobTitle() {
+    const connection = awaid db;
+
+}
+
+async function viewAllDepartments() {
+    const connection = awaid db;
+
+}
+
+async function addDepartment() {
+    const connection = awaid db;
+
+}
+
+promptUser(); 
